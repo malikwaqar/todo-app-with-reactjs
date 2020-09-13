cc51a4b97f5e40c25c45b9b19126b55c83bee91c
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-13 11:10:40 +0500

    Small CSS Changes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -21,9 +21,9 @@ function Todo(props) {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
     const [input, setInput] = useState();
-    const handleOpen = () => {
-        setOpen(true);
-    };
+    // const handleOpen = () => {
+    //     setOpen(true);
+    // };
     const updateTodo = () => {
         db.collection('todos').doc(props.todo.id).set({
             todo: input
@@ -34,9 +34,9 @@ function Todo(props) {
         <div>
         <Modal
         open = {open}
-        close = { e => setOpen(false) } > 
+        onClose = { e => setOpen(false) } > 
         <div className={classes.paper}>
-            <h1>Modal</h1>
+            <h1>Edit Todo</h1>
             <input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)} />
             <Button onClick= {updateTodo}>Update</Button>
         </div>
@@ -46,10 +46,10 @@ function Todo(props) {
                 <ListItemAvatar> 
                 </ListItemAvatar>
                 <ListItemText primary={props.todo.todo }></ListItemText>
-                
+                <DeleteIcon className="delete__button" onClick={event => db.collection('todos').doc(props.todo.id).delete() }></DeleteIcon>
+            <EditIcon className="edit__button" onClick = {e => setOpen(true)} />
             </ListItem>
-            <DeleteIcon onClick={event => db.collection('todos').doc(props.todo.id).delete() }></DeleteIcon>
-            <EditIcon onClick = {e => setOpen(true)} />
+            
         </List>
         </div>
     )
